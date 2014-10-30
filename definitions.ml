f84995a36b5a641b7b8c643b0efc131370065ba4
Ben Greenman <greenman.b@husky.neu.edu>
2014-10-29 23:47:56 -0400

    [def] method subtr imlemented, needs test

--- definitions.ml
+++ definitions.ml
@@ -29,7 +29,7 @@ type kind_context = (string * ctype * ctype) list
 (* 2014-10-29 the whole context might have more parts, later *)
 type context = kind_context
 let empty_context = []
-let rec context_lookup k ctx =
+let rec context_lookup (k:cname) (ctx:context) : ctype =
   match ctx with
   | [] -> failwith (Format.sprintf "unbound variable '%s'" k)
   | (k',_,tau_o)::tl -> if k = k' then tau_o else context_lookup k tl
@@ -45,13 +45,53 @@ let rec context_mem k ctx =
   match ctx with
   | [] -> false
   | (k',_,_)::tl -> k = k' || context_mem k tl
+let rec context_remove k ctx =
+  match ctx with
+  | [] -> []
+  | (k',_,_)::tl when k = k' -> tl
+  | hd::tl -> hd :: context_remove k tl
 let rec context_flip ctx =
   match ctx with
   | [] -> []
   | (k, tau_i, tau_o) :: tl -> (k, tau_o, tau_i) :: context_flip tl
 
-let and_all f xs = List.fold_left (fun acc x -> acc && f x) true  xs
-let or_all  f xs = List.fold_left (fun acc x -> acc || f x) false xs
+let rec subst (hole:vclass) (tau_i':vclass) (tau_o':vclass) : vclass =
+  begin
+    match hole with
+    | Top -> Top
+    | Bot -> Bot
+    | Class cr -> Class {cls = cr.cls
+                        ; tau_i = (subst cr.tau_i tau_o' tau_i')
+                        ; tau_o = (subst cr.tau_o tau_i' tau_o')
+                        }
+  end
+
+let rec vclass_of_ctype (c:ctype) (ctx:context) : vclass =
+  begin match c with
+    | TVar k  -> vclass_of_ctype (context_lookup k ctx) (context_remove k ctx)
+    | CType vc -> vc
+  end
+
+(*** collections ***)
+module MethodSet = Set.Make (struct
+  type t = cmethod
+  let compare (Method (_,n1,_,_)) (Method (_,n2,_,_)) = Pervasives.compare n1 n2
+end)
+(* Build a set of current & super methods -- these are what the current class might possibly override *)
+let rec collect_extends_methods_aux (acc:MethodSet.t) (c:vclass) : MethodSet.t =
+  begin
+    match c with
+    | Top | Bot -> acc
+    | Class cr  -> collect_extends_methods_aux
+                     (MethodSet.union acc (MethodSet.of_list cr.cls.methods))
+                     cr.cls.extends
+  end
+let collect_extends_methods (c:vclass) : MethodSet.t =
+  collect_extends_methods_aux MethodSet.empty c
+
+(*** misc ***)
+let and_all xs = List.fold_left (fun acc x -> acc && x) true  xs
+(* let or_all  f xs = List.fold_left (fun acc x -> acc || f x) false xs *)
 
 (*** inheritance + subtyping ***)
 (* record-only version would avoid some checks *)
@@ -85,16 +125,6 @@ let inherits_eq (c:vclass) (d:vclass) : vclass option =
     | _, _ -> inherits c d
   end
 
-let rec subst (hole:vclass) (tau_i':vclass) (tau_o':vclass) : vclass =
-  begin
-    match hole with
-    | Top -> Top
-    | Bot -> Bot
-    | Class cr -> Class {cls = cr.cls
-                        ; tau_i = (subst cr.tau_i tau_o' tau_i')
-                        ; tau_o = (subst cr.tau_o tau_i' tau_o')
-                        }
-  end
 let rec subtype (c:vclass) (d:vclass) : bool =
   begin
     match c, d with
@@ -122,25 +152,23 @@ let rec subtype (c:vclass) (d:vclass) : bool =
     | _, _ -> false
   end
 
-let subt_method (ctx:context) (m1:cmethod) (m2:cmethod) : bool =
-  failwith "not enough money for this"
-
-(*** collections ***)
-module MethodSet = Set.Make (struct
-  type t = cmethod
-  let compare (Method (_,n1,_,_)) (Method (_,n2,_,_)) = Pervasives.compare n1 n2
-end)
-(* Build a set of current & super methods -- these are what the current class might possibly override *)
-let rec collect_extends_methods_aux (acc:MethodSet.t) (c:vclass) : MethodSet.t =
-  begin
-    match c with
-    | Top | Bot -> acc
-    | Class cr  -> collect_extends_methods_aux
-                     (MethodSet.union acc (MethodSet.of_list cr.cls.methods))
-                     cr.cls.extends
-  end
-let collect_extends_methods (c:vclass) : MethodSet.t =
-  collect_extends_methods_aux MethodSet.empty c
+let subtype_method (ctx:context) (m1:cmethod) (m2:cmethod) : bool =
+  let Method (r1, n1, args1, _) = m1 in
+  let Method (r2, n2, args2, _) = m2 in
+  (* m1.returntype <: m2.returntype *)
+  let r1_t = vclass_of_ctype r1 ctx in
+  let r2_t = vclass_of_ctype r2 ctx in
+  let r_ok = subtype r1_t r2_t in
+  (* forall args, m2.arg <: m1.arg *)
+  let ctx' = context_flip ctx in
+  let a_ok =
+    ((List.length args1) = (List.length args2))
+    && (List.fold_left2 (fun acc (Arg (a1,_)) (Arg (a2,_)) ->
+                         acc && subtype (vclass_of_ctype a2 ctx')
+                                        (vclass_of_ctype a1 ctx'))
+                       true args1 args2)
+  in
+  and_all [r_ok; a_ok]
 
 (*** well-formedness ***)
 (* trivial now, may want to check uniqueness later *)
@@ -155,9 +183,9 @@ let rec class_ok_aux (ctx:context) (c:vclass) : bool =
        let ctx' = context_add (c'.cls.param) (CType c'.tau_i) (CType c'.tau_o) ctx in
        let p_ok = param_ok ctx' c' in
        let e_ok = class_ok_aux ctx' (c'.cls.extends) in
-       let f_ok = and_all (field_ok ctx') (c'.cls.fields) in
+       let f_ok = and_all (List.map (field_ok ctx') (c'.cls.fields)) in
        let m_ok = methods_ok ctx' c' in
-       and_all (fun x -> x) [p_ok; e_ok; f_ok; m_ok]
+       and_all [p_ok; e_ok; f_ok; m_ok]
   end
 and arg_ok (ctx:context) (a:carg) : bool =
   match a with | Arg (t, name) -> type_ok ctx t
@@ -173,9 +201,9 @@ and methods_ok (ctx:context) (c:class_record) : bool =
      let Method (ret_type, name, args, body) = m in
      acc
      && type_ok ctx ret_type
-     && and_all (arg_ok ctx) args
+     && and_all (List.map (arg_ok (context_flip ctx)) args)
      && if MethodSet.mem m extm_set
-        then subt_method ctx m (MethodSet.find m extm_set)
+        then subtype_method ctx m (MethodSet.find m extm_set)
         else true
     ) true c.cls.methods
 and type_ok (ctx:context) (t:ctype) : bool =
