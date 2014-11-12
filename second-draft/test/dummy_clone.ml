bf895d1516c759ecb3f2a576c3a524d1facb016e
Ben Greenman <greenman.b@husky.neu.edu>
2014-11-11 19:10:40 -0500

    [test] dummy clone, a little more readable

--- second-draft/test/dummy_clone.ml
+++ second-draft/test/dummy_clone.ml
@@ -17,15 +17,6 @@ let c_black_box =
                     , Return Null))
         ])
 
-let () = (* quick test, assert that black box instances do not have a .equals *)
-  (* Check well-formed *)
-  let ctx = Context.init [C c_black_box] [] [] in
-  let () = typecheck ctx (C c_black_box) in
-  (* Assert methods *)
-  let () = check_method_names ctx ~expected:["doesNothing"]
-                              ~observed:(Instance("BlackBox", [])) in
-  ()
-
 (* Shape shifter: just allocate space for the new thing, don't actually clone it *)
 let w_dummy_clone =
   Shifter ( "DummyClone"
@@ -34,6 +25,16 @@ let w_dummy_clone =
           )
 
 let () = (* Make Iterable<BlackBox> with and without RefEqual *)
+  let empty_ctx = Context.init [C c_black_box] [] [] in
+  (* Check well-formed, check methods *)
+  let () = typecheck empty_ctx (C c_black_box) in
+  let () = check_method_names empty_ctx ~expected:["doesNothing"]
+                              ~observed:(Instance("BlackBox", [])) in
+  (* Make sure cloning fails *)
+  let () = check_expr_false empty_ctx ~expected:(Instance("BlackBox", []))
+                      ~observed:(Call(("BlackBox", []), "clone", []))
+  in
+  (* Add shifter, try cloning *)
   let ctx =
     let cc = [C c_black_box] in
     let sc = [("BlackBox", w_dummy_clone)] in
@@ -43,5 +44,5 @@ let () = (* Make Iterable<BlackBox> with and without RefEqual *)
   let () = check_expr ctx ~expected:(Instance("BlackBox", []))
                       ~observed:(Call(("BlackBox", []), "clone", []))
   in
-  let () = Format.printf "%s\n" (Pretty_print.string_of_shifter_t ctx w_dummy_clone) in
+  let () = Format.printf "%s\n" (Pretty_print.string_of_shifter_t empty_ctx w_dummy_clone) in
   ()
